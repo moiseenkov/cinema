418471a1849d8ecb0135451a448d91e97b80bd6a
Maksim Moiseenkov <moiseenkov.ms@gmail.com>
2019-11-22 11:16:12 +0500

    Negative tests for /token/ url

--- booking/tests/test_url_token.py
+++ booking/tests/test_url_token.py
@@ -42,3 +42,44 @@ class LoginURLPositiveTestCase(LoggedInTestCase):
             with self.subTest(cred=cred):
                 response = self.client.delete(path=self.url_login, data=cred)
                 self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class LoginURLNegativeTestCase(LoggedInTestCase):
+    def setUp(self) -> None:
+        self.url_login = reverse('token')
+        super(LoginURLNegativeTestCase, self).setUp()
+
+    def test_url_login_negative_POST_no_password(self):
+        credentials = {
+            'email': self.credentials[0]['email']
+        }
+        response = self.client.post(path=self.url_login, data=credentials)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_url_login_negative_POST_no_email(self):
+        credentials = {
+            'password': self.credentials[0]['password']
+        }
+        response = self.client.post(path=self.url_login, data=credentials)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_url_login_negative_POST_wrong_email(self):
+        credentials = self.credentials[0].copy()
+        credentials['email'] = 'another@test.com'
+
+        response = self.client.post(path=self.url_login, data=credentials)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_url_login_negative_POST_wrong_password(self):
+        credentials = self.credentials[0].copy()
+        credentials['password'] = 'wrong password'
+
+        response = self.client.post(path=self.url_login, data=credentials)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_url_login_negative_POST_incorrect_email(self):
+        credentials = self.credentials[0].copy()
+        credentials['email'] = r'incorrect email'
+
+        response = self.client.post(path=self.url_login, data=credentials)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
