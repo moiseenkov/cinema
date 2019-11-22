b7b47283bf9b3d10000cbfc7d5119e326ef5870e
Maksim Moiseenkov <moiseenkov.ms@gmail.com>
2019-11-22 13:04:34 +0500

    Fixed bug in LoggedInTestCase, UserForTesting

--- booking/tests/helper.py
+++ booking/tests/helper.py
@@ -8,37 +8,33 @@ class UserForTesting:
     def __init__(self, **kwargs):
         self.credentials = kwargs
         if kwargs.get('is_staff', None):
-            self.user = CustomUser.objects.create_user(**kwargs)
-        else:
             self.user = CustomUser.objects.create_superuser(**kwargs)
+        else:
+            self.user = CustomUser.objects.create_user(**kwargs)
         self.user.save()
 
 
 class LoggedInTestCase(TestCase):
-    def _get_token(self, user):
-        data = {
-            'email': user.email,
-            'password': user.password
-        }
-        response = self.client.put(path=reverse('token'), data=data)
+    def _get_token(self, credentials):
+        response = self.client.post(path=reverse('token'), data=credentials)
         return response.data.get('access', None)
 
     def setUp(self) -> None:
-        user_credentials = {
+        self.user_credentials = {
             'email': 'user@test.com',
             'password': 'user_password',
         }
-        admin_credentials = {
+        self.admin_credentials = {
             'email': 'admin@test.com',
             'password': 'admin_password',
             'is_staff': True
         }
-        self.user = UserForTesting(**user_credentials).user
-        self.admin = UserForTesting(**admin_credentials).user
-        self.user_token = self._get_token(self.user)
-        self.admin_token = self._get_token(self.admin)
+        self.user = UserForTesting(**self.user_credentials).user
+        self.admin = UserForTesting(**self.admin_credentials).user
+        self.user_token = self._get_token(self.user_credentials)
+        self.admin_token = self._get_token(self.admin_credentials)
         self.credentials = [
-            user_credentials,
-            admin_credentials,
+            self.user_credentials,
+            self.admin_credentials,
         ]
         super(LoggedInTestCase, self).setUp()
