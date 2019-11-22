4fc1a4832b1709569b8940d04be672ea8c0f6db6
Maksim Moiseenkov <moiseenkov.ms@gmail.com>
2019-11-22 17:23:53 +0500

    Positive tests for /users/ in UsersListURLPositiveTestCase

--- booking/tests/test_url_users.py
+++ booking/tests/test_url_users.py
@@ -240,3 +240,91 @@ class UsersDetailURLNegativeTestCase(LoggedInTestCase):
         response = self.client.put(path=url, data=data, content_type='application/json',
                                    HTTP_AUTHORIZATION=f'Bearer {self.user_token}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UsersListURLPositiveTestCase(LoggedInTestCase):
+    def setUp(self) -> None:
+        self.url_list = reverse('user-list')
+        self.another_user_credentials = {
+            'email': 'another_email@test.com',
+            'password': 'password',
+        }
+        super(UsersListURLPositiveTestCase, self).setUp()
+
+    def test_url_users_list_positive_GET_user(self):
+        response = self.client.get(path=self.url_list, HTTP_AUTHORIZATION=f'Bearer {self.user_token}')
+        expected_result = {
+            'id': self.user.pk,
+            'email': self.user.email,
+        }
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data.get('results', None))
+        data = response.data['results']
+        self.assertEqual(len(data), 1)
+        self.assertDictEqual(dict(data[0]), expected_result)
+
+    def test_url_users_list_positive_GET_admin(self):
+        response = self.client.get(path=self.url_list, HTTP_AUTHORIZATION=f'Bearer {self.admin_token}')
+        expected_result = [
+            {
+                'id': self.user.pk,
+                'email': self.user.email,
+                'is_active': True,
+                'is_staff': False,
+                'password': self.user.password,
+            },
+            {
+                'id': self.admin.pk,
+                'email': self.admin.email,
+                'is_active': True,
+                'is_staff': True,
+                'password': self.admin.password,
+            },
+        ]
+        expected_result.sort(key=lambda user: user['id'], reverse=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(response.data.get('results', None))
+        data = response.data['results']
+        self.assertEqual(len(data), 2)
+        for given_user, expected_user in zip(data, expected_result):
+            with self.subTest(given_user=given_user, expected_user=expected_user):
+                self.assertDictEqual(given_user, expected_user)
+
+    def test_url_users_list_positive_POST_unauthorized(self):
+        email = self.another_user_credentials['email']
+        password = self.another_user_credentials['password']
+        response = self.client.post(path=self.url_list, data=self.another_user_credentials)
+
+        user = CustomUser.objects.filter(email=email).first()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.email, email)
+        self.assertNotEqual(user.password, password)  # password should be hashed
+        self.assertTrue(user.check_password(password))
+        expected_response = {
+            'id': user.pk,
+            'email': user.email,
+            'password': user.password,
+        }
+        self.assertDictEqual(response.data, expected_response)
+
+    def test_url_users_list_positive_POST_admin(self):
+        email = self.another_user_credentials['email']
+        password = self.another_user_credentials['password']
+        response = self.client.post(path=self.url_list, data=self.another_user_credentials,
+                                    HTTP_AUTHORIZATION=f'Bearer {self.admin_token}')
+
+        new_user = CustomUser.objects.filter(email=email).first()
+        self.assertIsNotNone(new_user)
+        self.assertEqual(new_user.email, email)
+        self.assertNotEqual(new_user.password, password)  # password should be hashed
+        self.assertTrue(new_user.check_password(password))
+
+        expected_response = {
+            'id': new_user.pk,
+            'email': new_user.email,
+            'password': new_user.password,
+            'is_staff': False,
+            'is_active': True,
+        }
+        self.assertDictEqual(response.data, expected_response)
